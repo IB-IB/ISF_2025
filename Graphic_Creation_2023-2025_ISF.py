6ea26d04a32ecf8f1844457366f6a0d36c7aa06c
IB-IB <ibradfo@purdue.edu>
2025-10-10 12:38:48 -0500

    Friday Update, Graph making

--- Graphic_Creation_2023-2025_ISF.py
+++ Graphic_Creation_2023-2025_ISF.py
@@ -11,8 +11,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 #import matplotlib.dates as mdates
-#import numpy as np
-#import scipy as sp
+import numpy as np
+import scipy as sp
 
 df25 = pd.read_csv('2025_Data_set_M.csv') #open a csv within GH folder for data process.
 df24 = pd.read_csv('2024_Data_set_M.csv') #open a csv within GH folder for data process.
@@ -50,9 +50,39 @@ df25_sd = df25.dropna(subset='Secchi Depth (m)')
 gz = sns.jointplot(x='Secchi Depth (m)', y='Chla (ug/L)', data=df25_sd, kind='reg')
 gz.set_axis_labels(xlabel='Secchi Depth (m)', ylabel='Chla (ug/L)')
 plt.suptitle("Secchi Depth vs. Chla, 2025 Mississinewa")
-#r, p = sp.stats.pearsonr(df_tur['TURBIDITY FNU'], df_tur['Chla (ug/L)'])
-#ax = plt.gca()
-#ax.text(.05, .8, 'r={:.2f}'.format(r), transform=ax.transAxes)
+r, p = sp.stats.pearsonr(df25_sd['Secchi Depth (m)'], df25_sd['Chla (ug/L)'])
+ax = plt.gca()
+ax.text(.05, .8, 'r={:.2f}'.format(r), transform=ax.transAxes)
 #plt.savefig(outFileName) # saves the graph into the github folder
 plt.savefig('Secchi.png', dpi = 400)
 plt.show()
+
+"""WORK HERE"""
+plt.figure(figsize=(12,6)) #space out recording
+# https://stackoverflow.com/questions/68881330/seaborn-plot-with-two-y-axis
+ax1 = plt.subplot()
+ax2 = ax1.twinx()
+adgb = sns.scatterplot(x='Date', y='Chla (ug/L)', data=df25, ax=ax1, legend ='auto') 
+adgb = sns.scatterplot(x='Date', y='TP BDL', data=df25, color='red', ax=ax2, markers='s',legend ='auto')
+adgb = sns.scatterplot(x='Date', y='Nox BDL', data=df25, color= 'orange', ax=ax2, markers='s',legend ='auto')
+ax.legend(labels=['Chla', 'TP', 'NOx'])
+ax2.tick_params(axis='y', colors='red')
+adgb.set_xlabel('Date')
+#https://stackoverflow.com/questions/31632637/label-axes-on-seaborn-barplot
+ax2.set(ylabel='TP, NOx (ppm)')
+adgb.set_title("Average Chl-a Recorded Distance from the Dam over Summer 2024")
+plt.savefig('WIP_TP_NOx_CHLA.png', dpi = 400)
+plt.show()
+
+"""
+gz = sns.jointplot(x='Secchi Depth (m)', y='Chla (ug/L)', data=df25_sd, kind='reg')
+gz.set_axis_labels(xlabel='Secchi Depth (m)', ylabel='Chla (ug/L)')
+plt.suptitle("Secchi Depth vs. Chla, 2025 Mississinewa")
+r, p = sp.stats.pearsonr(df25_sd['Secchi Depth (m)'], df25_sd['Chla (ug/L)'])
+ax = plt.gca()
+ax.text(.05, .8, 'r={:.2f}'.format(r), transform=ax.transAxes)
+#plt.savefig(outFileName) # saves the graph into the github folder
+plt.savefig('Secchi.png', dpi = 400)
+plt.show()
+"""
+
